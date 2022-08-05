68ed882ec6faae0407c6fefb476aa2e6f8016cbd
Zoe Rodriguez <zoe.rod9767@gmail.com>
2022-08-04 19:44:16 -0500

    go to homeScreen from searchScreen

--- controller/SearchScreenController.java
+++ controller/SearchScreenController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import model.Model;
 
 public class SearchScreenController {
@@ -14,7 +21,12 @@ public class SearchScreenController {
         greeting.setText(Model.getGreeting());
     }
 
-    // public void addToOrder(ActionEvent action){
+    public void goToHome(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/homeScreen.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 800);
+        stage.setScene(scene);
+        stage.show();
         
-    // }
+    }
 }
